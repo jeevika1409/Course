f797c8aa984923552a5e0e58acf40f634731ec08
jeevika1409 <jeevikacp14@gmail.com>
2025-05-13 09:27:46 +0530

    save

--- Course.java
+++ Course.java
@@ -28,6 +28,7 @@ class Student {
     }
 
     public void displayStudentInfo() {
+        System.out.println("Student Information:");
         System.out.println("Name: " + name);
         System.out.println("Program: " + program);
         System.out.println("Semester: " + semester);
